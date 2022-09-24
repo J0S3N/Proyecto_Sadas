051e5831e61b6108aa374234274c6f8c0aa15d13
José Vargas Ramírez <80367699+J0S3N@users.noreply.github.com>
2022-09-24 10:37:04 -0600

    Actualizacion v1SadasSQLQuery.sql

--- v1SadasSQLQuery.sql
+++ v1SadasSQLQuery.sql
@@ -188,6 +188,26 @@ CREATE TABLE [dbo].[centro_educativo](
 ) ON [PRIMARY]
 GO
 
+USE [v1Sadas]
+GO
+
+/****** Object:  Table [dbo].[permiso]    Script Date: 21/9/2022 18:10:39 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[permiso](
+	[id_permiso] [int] IDENTITY(1,1) NOT NULL,
+	[nombe] [nvarchar](50) NULL,
+	[detalle] [nvarchar](500) NULL,
+ CONSTRAINT [PK_permiso] PRIMARY KEY CLUSTERED 
+(
+	[id_permiso] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
 
 USE [v1Sadas]
 GO
@@ -304,28 +324,6 @@ CREATE TABLE [dbo].[historico](
 	[id_historico] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-
-
-USE [v1Sadas]
-GO
-
-/****** Object:  Table [dbo].[permiso]    Script Date: 21/9/2022 18:10:39 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[permiso](
-	[id_permiso] [int] IDENTITY(1,1) NOT NULL,
-	[nombe] [nvarchar](50) NULL,
-	[detalle] [nvarchar](500) NULL,
- CONSTRAINT [PK_permiso] PRIMARY KEY CLUSTERED 
-(
-	[id_permiso] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
 GO
 
 
